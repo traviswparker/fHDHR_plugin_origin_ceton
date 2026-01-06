d3293cf3cefab6b7f3bc84c897077bd0d9b00ab3
Travis Parker <traviswparker@users.noreply.github.com>
2026-01-06 17:12:33 -0500

    refactor plugin method names and web ui
    get_ceton_getvar is now get_ceton_var, remove redundant devinuse in web

--- web/ceton_html.py
+++ web/ceton_html.py
@@ -21,57 +21,34 @@ class Ceton_HTML():
         self.template = StringIO()
         self.template.write(open(self.template_file).read())
 
-        self.hwtype = self.plugin_utils.origin_obj.get_ceton_getvar(0, "HostConnection")
-        if 'pci' in self.hwtype:
-            self.ceton_pcie = True
-        else:
-            self.ceton_pcie = False
-
     def __call__(self, *args):
         return self.get(*args)
 
-    def devinuse(self, instance):
-        filename = self.plugin_utils.origin_obj.tunerstatus[str(instance)]['streamurl']
-        if '/dev' in filename:
-            try:
-                subprocess.check_output(['fuser', filename], stderr=subprocess.DEVNULL)
-                # man: if access has been found, fuser returns zero
-                # => Return True, device is in use
-                return "In Use"
-            except subprocess.CalledProcessError:
-                # man: fuser returns a non-zero return code if none of the specified files is accessed
-                # => Return False, device is not in use
-                return "Available"
-        else:
-            # Not PCIe card, so don't check device
-            return "Not PCIe Card"
-
     def get(self, *args):
-
         if self.origin_obj.setup_success:
             origin_status_dict = {"Devices": len(self.fhdhr.config.dict["ceton"]["device_tuners"])}
             for i in range(len(self.fhdhr.config.dict["ceton"]["device_tuners"])):
                 device = self.plugin_utils.origin_obj.tunerstatus[str(i)]['ceton_ip']
                 origin_status_dict["Device"+str(i)] = {}
                 origin_status_dict["Device"+str(i)]["Setup"] = "Success"
-                origin_status_dict["Device"+str(i)]["Temp"] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Temperature")
-                origin_status_dict["Device"+str(i)]["HWType"] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "HostConnection")
-                origin_status_dict["Device"+str(i)]["HostHardware"] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "HostHardware")
-                origin_status_dict["Device"+str(i)]["HostFirmware"] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "HostFirmware")
-                origin_status_dict["Device"+str(i)]["HostSerial"] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "HostSerial")
+                origin_status_dict["Device"+str(i)]["Temp"] = self.plugin_utils.origin_obj.get_ceton_var(i, "Temperature")
+                origin_status_dict["Device"+str(i)]["HWType"] = self.plugin_utils.origin_obj.get_ceton_var(i, "HostConnection")
+                origin_status_dict["Device"+str(i)]["HostHardware"] = self.plugin_utils.origin_obj.get_ceton_var(i, "HostHardware")
+                origin_status_dict["Device"+str(i)]["HostFirmware"] = self.plugin_utils.origin_obj.get_ceton_var(i, "HostFirmware")
+                origin_status_dict["Device"+str(i)]["HostSerial"] = self.plugin_utils.origin_obj.get_ceton_var(i, "HostSerial")
 
             for i in range(int(self.fhdhr.config.dict["ceton"]["tuners"])):
                 origin_status_dict["Tuner"+str(i)] = {}
                 origin_status_dict["Tuner"+str(i)]['Device'] = int(self.plugin_utils.origin_obj.tunerstatus[str(i)]['ceton_device'])
-                origin_status_dict["Tuner"+str(i)]['Transport'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "TransportState")
-                origin_status_dict["Tuner"+str(i)]['HWState'] = self.devinuse(i)
-                origin_status_dict["Tuner"+str(i)]['Channel'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Signal_Channel")
-                origin_status_dict["Tuner"+str(i)]['SignalLock'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "SignalCarrierLock")
-                origin_status_dict["Tuner"+str(i)]['PCRLock'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "SignalPCRLock")
-                origin_status_dict["Tuner"+str(i)]['Signal'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Signal_Level")
-                origin_status_dict["Tuner"+str(i)]['SNR'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Signal_SNR")
-                origin_status_dict["Tuner"+str(i)]['BER'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Signal_BER")
-                origin_status_dict["Tuner"+str(i)]['Modulation'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Signal_Modulation")
+                origin_status_dict["Tuner"+str(i)]['Transport'] = self.plugin_utils.origin_obj.get_ceton_var(i, "TransportState")
+                origin_status_dict["Tuner"+str(i)]['HWState'] = self.plugin_utils.origin_obj.devinuse(i)
+                origin_status_dict["Tuner"+str(i)]['Channel'] = self.plugin_utils.origin_obj.get_ceton_var(i, "Signal_Channel")
+                origin_status_dict["Tuner"+str(i)]['SignalLock'] = self.plugin_utils.origin_obj.get_ceton_var(i, "SignalCarrierLock")
+                origin_status_dict["Tuner"+str(i)]['PCRLock'] = self.plugin_utils.origin_obj.get_ceton_var(i, "SignalPCRLock")
+                origin_status_dict["Tuner"+str(i)]['Signal'] = self.plugin_utils.origin_obj.get_ceton_var(i, "Signal_Level")
+                origin_status_dict["Tuner"+str(i)]['SNR'] = self.plugin_utils.origin_obj.get_ceton_var(i, "Signal_SNR")
+                origin_status_dict["Tuner"+str(i)]['BER'] = self.plugin_utils.origin_obj.get_ceton_var(i, "Signal_BER")
+                origin_status_dict["Tuner"+str(i)]['Modulation'] = self.plugin_utils.origin_obj.get_ceton_var(i, "Signal_Modulation")
         else:
             origin_status_dict = {"Setup": "Failed"}
 
